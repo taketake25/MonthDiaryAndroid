a4e53a61c94fa89a7c391c29e518b0c878e9c595
taketake25 <takeru31415@gmail.com>
2018-05-06 19:59:11 +0900

    import libraries and write mainActivity base

--- app/src/main/java/com/example/bbbt/monthdiaryandroid/MainActivity.kt
+++ app/src/main/java/com/example/bbbt/monthdiaryandroid/MainActivity.kt
@@ -3,3 +3,13 @@ package com.example.bbbt.monthdiaryandroid
 /**
  * Created by Takeru Baba on 2018/05/06.
  */
+
+import android.app.Activity
+import android.os.Bundle
+
+class MainActivity() : Activity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main)
+    }
+}
